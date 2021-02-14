c57dac63d724bc08f217cab5bb9fd58b209cac8c
Shobhit Behl <shobhitbehl98@gmail.com>
2021-02-14 20:30:48 +0530

    ladwa

--- Web_Scrapping/activity/match.js
+++ Web_Scrapping/activity/match.js
@@ -16,9 +16,8 @@ function getdata(data){
     let ch=cheerio.load(data);
     let bothinnings=ch('.card.content-block.match-scorecard-table .Collapsible');
     for(let i=0;i<bothinnings.length;i++){
-        let teamname=ch(bothinnings[i]).find("h5").text();
-        teamname=teamname.split("INNINGS")[0].trim();
-        console.log(teamname);
+        let teamname=ch(bothinnings[i]).find("h5").text().split("INNINGS")[0].trim();      
+        let  opponentteam=ch(bothinnings[i==0?1:0]).find("h5").text().split("INNINGS")[0].trim();
         let alltrs=ch(bothinnings[i]).find(".table.batsman tbody tr");
         for(let i=0;i<alltrs.length-1;i++){
             let alltds=ch(alltrs[i]).find("td");
@@ -26,22 +25,76 @@ function getdata(data){
 
             if(alltds.length>1){
                 let batsmanname=ch(alltds[0]).text().trim();
-                let dismissal=ch(alltds[1]).text().trim();
+                // let dismissal=ch(alltds[1]).text().trim();
                 let runs=ch(alltds[2]).text().trim();
                 let balls=ch(alltds[3]).text().trim();
                 let fours=ch(alltds[5]).text().trim();
                 let sixes=ch(alltds[6]).text().trim();
                 let sr=ch(alltds[7]).text().trim();
-                console.log(`Batsman = ${batsmanname} Dismissal = ${dismissal} Runs = ${runs} Balls= ${balls} Fours = ${fours} Sixes= ${sixes} Strike Rate= ${sr}` );
+                processlog(teamname,opponentteam,batsmanname,runs,balls,fours,sixes,sr);
+                // console.log(`Batsman = ${batsmanname} Dismissal = ${dismissal} Runs = ${runs} Balls= ${balls} Fours = ${fours} Sixes= ${sixes} Strike Rate= ${sr}` );
             }
         }
         console.log("###########################################################################################");
     }
-    console.log(" ");
-    console.log("New Match");
-    console.log(" ");
-
 
 }
+
+function checkTeamFolder(teamPath){
+return fs.existsSync(teamPath);
+}
+function checkBatsmanFile(batsmanPath){
+return fs.existsSync(batsmanPath);
+}
+function updateBatsmanFile(batsmanPath,opponentteam,runs,balls,fours,sixes,sr){
+let batsmanfile=fs.readFileSync(batsmanPath);
+batsmanfile=JSON.parse(batsmanfile);
+let inning={
+    Opponent:opponentteam,
+    Runs:runs,
+    Balls:balls,
+    Fours:fours,
+    Sixes:sixes,
+    Strike_Rate:sr
+}
+batsmanfile.push(inning);
+fs.writeFileSync(batsmanPath,JSON.stringify(batsmanfile));
+}
+function createBatsman(batsmanPath,opponentteam,runs,balls,fours,sixes,sr){
+    let batsmanfile=[];
+    let inning={
+        Opponent:opponentteam,
+        Runs:runs,
+        Balls:balls,
+        Fours:fours,
+        Sixes:sixes,
+        Strike_Rate:sr
+    }
+    batsmanfile.push(inning);
+    fs.writeFileSync(batsmanPath,JSON.stringify(batsmanfile));
+
+}
+function createTeamFolder(teamPath){
+fs.mkdirSync(teamPath);
+}
+
+function processlog(teamname,opponentteam,batsmanname,runs,balls,fours,sixes,sr){
+    let teamPath=`./IPL/${teamname}`;
+    let batsmanPath=`./IPL/${teamname}/${batsmanname}.json`;
+    let isTeamFolder=checkTeamFolder(teamPath);
+    if(isTeamFolder){
+        let isBatsman=checkBatsmanFile(batsmanPath);
+       if(isBatsman){
+           updateBatsmanFile(batsmanPath,opponentteam,runs,balls,fours,sixes,sr);
+       }else{
+           createBatsman(batsmanPath,opponentteam,runs,balls,fours,sixes,sr);
+       }
+    }else{
+      createTeamFolder(teamPath);
+      createBatsman(batsmanPath,opponentteam,runs,balls,fours,sixes,sr);
+    }
+}
+
+
 
 module.exports = getMatch;
